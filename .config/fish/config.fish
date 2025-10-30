0b9d7c79d8d1ee198e395def0c79ffcc26401141
Duaxedapkhuyettat <hifumidaisuki.affair808@passinbox.com>
2025-10-31 01:10:59 +0700

    +1 Oh my posh to bashrc & fish.config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,5 @@ if status is-interactive
     zoxide init fish | source
     set -gx MICRO_TRUECOLOR 1
     set -gx EDITOR micro
+    oh-my-posh init fish --config ~/.config/catppuccin_mocha.omp.json | source
 end
